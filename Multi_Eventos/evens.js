78be7495acd99e1c45f2f5ea5076eec0ec4b8182
Jefferson David Palacio Lozada <davidpalacio1506@outlook.com>
2025-04-29 14:43:04 -0500

    Multi eventos

---
+++ Multi_Eventos/evens.js
@@ -0,0 +1,10 @@
+"use strict";
+function imprimir() {
+    console.log("Hola");
+    let elemnto = document.getElementById("selecioname");
+    elemnto.innerHTML = "<b><p>chupalo cristian </p> </b>";
+}
+function imprimir1() {
+    let a = document.getElementById("selecioname");
+    a.innerText = 'chupalo cristian';
+}
---
+++ Multi_Eventos/evens.ts
@@ -0,0 +1,12 @@
+function imprimir():void{
+    console.log("Hola")
+   let elemnto= document.getElementById("selecioname") as HTMLParagraphElement ;
+ elemnto.innerHTML = "<b><p>chupalo cristian </p> </b>";
+
+}
+
+function imprimir1(): void {
+ 
+let a= document.getElementById("selecioname") as HTMLParagraphElement;
+a.innerText='chupalo cristian'
+}
