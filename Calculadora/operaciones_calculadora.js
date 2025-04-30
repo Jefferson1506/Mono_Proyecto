41a8fcc5e070fa3fc44e39eb364d407f0571fffe
Jefferson David Palacio Lozada <davidpalacio1506@outlook.com>
2025-04-30 16:31:11 -0500

    Calculadora completa (Basica)

--- Calculadora/operaciones_calculadora.js
+++ Calculadora/operaciones_calculadora.js
@@ -1,14 +1,51 @@
 "use strict";
+window.onload = () => {
+    botones();
+};
 function agregar(valor) {
-    let pantalla = document.getElementById("pantalla");
+    const pantalla = document.getElementById("pantalla");
     pantalla.value += " " + valor;
 }
 function calcular() {
-    let valorPantalla = document.getElementById("pantalla");
-    const resultado = eval(valorPantalla.value);
-    valorPantalla.value = resultado;
+    const pantalla = document.getElementById("pantalla");
+    pantalla.value = eval(pantalla.value);
 }
 function limpiarPantalla() {
     const pantalla = document.getElementById("pantalla");
     pantalla.value = "";
+}
+function botones() {
+    const botones = [
+        "1",
+        "2",
+        "3",
+        "+",
+        "4",
+        "5",
+        "6",
+        "-",
+        "7",
+        "8",
+        "9",
+        "*",
+        "0",
+        ".",
+        "/",
+        "=",
+        "C",
+    ];
+    const contenedor = document.getElementById("botones");
+    botones.forEach((valor) => {
+        const boton = document.createElement("button");
+        boton.innerText = valor;
+        boton.onclick = () => {
+            if (valor === "C")
+                limpiarPantalla();
+            else if (valor === "=")
+                calcular();
+            else
+                agregar(valor);
+        };
+        contenedor.appendChild(boton);
+    });
 }
--- Calculadora/operaciones_calculadora.ts
+++ Calculadora/operaciones_calculadora.ts
@@ -1,15 +1,55 @@
+window.onload = () => {
+  botones();
+};
+
 function agregar(valor: string): void {
-  let pantalla = document.getElementById("pantalla") as HTMLInputElement;
-  pantalla.value += " " + valor;
+  const pantalla = document.getElementById("pantalla") as HTMLInputElement;
+  pantalla.value += valor;
 }
 
 function calcular(): void {
-  let valorPantalla = document.getElementById("pantalla") as HTMLInputElement;
-  const resultado = eval(valorPantalla.value);
-  valorPantalla.value = resultado;
+  const pantalla = document.getElementById("pantalla") as HTMLInputElement;
+
+  pantalla.value = eval(pantalla.value);
 }
 
 function limpiarPantalla(): void {
   const pantalla = document.getElementById("pantalla") as HTMLInputElement;
   pantalla.value = "";
+}
+
+function botones(): void {
+  const botones = [
+    "1",
+    "2",
+    "3",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "7",
+    "8",
+    "9",
+    "*",
+    "0",
+    ".",
+    "/",
+    "=",
+    "C",
+  ];
+
+  const contenedor = document.getElementById("botones") as HTMLButtonElement;
+
+  botones.forEach((valor: string) => {
+    const boton = document.createElement("button") as HTMLButtonElement;
+    boton.innerText = valor;
+
+    boton.onclick = () => {
+      if (valor === "C") limpiarPantalla();
+      else if (valor === "=") calcular();
+      else agregar(valor);
+    };
+    contenedor.appendChild(boton);
+  });
 }
